788b41a84ca9ba726ca0a1c6d5149b90d79eaf75
Vinicius Prates <viniciusprates.6798@aluno.saojudas.br>
2020-03-15 16:44:18 -0300

    Classe figura parcialmente completa

--- Tarefa2/Figura.java
+++ Tarefa2/Figura.java
@@ -1,3 +1,14 @@
 public abstract class Figura {
-    
+    private String nome;
+
+    public Figura(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public abstract double area();
 }
