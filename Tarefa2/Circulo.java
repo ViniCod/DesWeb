d676e517bac759dd97d5dcda5299663d7a35d924
Vinicius Prates <viniciusprates.6798@aluno.saojudas.br>
2020-03-15 19:03:23 -0300

    Alterações em Figura, Circulo e Poligono

--- Tarefa2/Circulo.java
+++ Tarefa2/Circulo.java
@@ -1,15 +1,21 @@
 public class Circulo extends Figura {
-    private double area;
-
-    public Circulo(String nome, double area){
-        super(nome);
-        this.area = area;
-    }
-    public void setArea(double area){
-        this.area = area;
-    }
-    @Override
-    public double area(){
-        return area;
-    }
+	private double raio;
+	
+	public Circle(double raio) {
+		this.setRaio(raio);
+	}
+	public double getRaio() {
+		return raio;
+	}
+	public void setRaio(double raio) {
+		this.radius = radius;
+	}
+	@Override
+	public double area() {
+		return Math.PI * this.getRaio();
+	}
+	@Override
+	public double perimetro() {
+		return 2 * Math.PI * this.getRaio();
+	}
 }
--- Tarefa2/Figura.java
+++ Tarefa2/Figura.java
@@ -1,14 +1,4 @@
 public abstract class Figura {
-    private String nome;
-
-    public Figura(String nome){
-        this.nome = nome;
-    }
-    public String getNome(){
-        return nome;
-    }
-    public void setNome(String nome){
-        this.nome = nome;
-    }
     public abstract double area();
+    public abstract double perimetro();
 }
