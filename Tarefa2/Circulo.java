ed40cbf5d041de50fde32a0070905b9a6ccaed8f
Vinicius Prates <viniciusprates.6798@aluno.saojudas.br>
2020-03-15 16:57:04 -0300

    Classe circulo concluída sem teste

--- Tarefa2/Circulo.java
+++ Tarefa2/Circulo.java
@@ -1,3 +1,15 @@
 public class Circulo extends Figura {
-    
+    private double area;
+
+    public Circulo(String nome, double area){
+        super(nome);
+        this.area = area;
+    }
+    public void setArea(double area){
+        this.area = area;
+    }
+    @Override
+    public double area(){
+        return area;
+    }
 }
