380fbcb88d8c4dcbfbee7f25dc048a8d7e7ac206
Vinicius Prates <viniciusprates.6798@aluno.saojudas.br>
2020-04-28 22:13:09 -0300

    modificação na classe ConnectionFactory

--- Tarefa6/src/dao/ConnectionFactory.java
+++ Tarefa6/src/dao/ConnectionFactory.java
@@ -16,7 +16,7 @@ public class ConnectionFactory {
 	// Obtém conexão com o banco de dados
 	public static Connection obtemConexao() throws SQLException {
 		return DriverManager
-				.getConnection("jdbc:mysql://localhost:3306/Tarefa3?useTimezone=true&serverTimezone=America/Sao_Paulo&user=root&password=");
+				.getConnection("jdbc:mysql://localhost:3306/Tarefa3?useTimezone=true&serverTimezone=America/Sao_Paulo&user=root");
 	}
 
 }
