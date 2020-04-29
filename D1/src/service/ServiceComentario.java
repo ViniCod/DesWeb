f6fe44c0b7383ba57cafa0593a77ba67520d28d2
Vinicius Prates <viniciusprates.6798@aluno.saojudas.br>
2020-04-29 15:48:37 -0300

    Ateração na classe de serviço Comentario

--- D1/src/service/ServiceComentario.java
+++ D1/src/service/ServiceComentario.java
@@ -7,11 +7,11 @@ import model.Comentario;
 
 public class ServiceComentario {
 	private DAOComentario daoComentario = new DAOComentario();
-	
-	public int create(Comentario comentario) {
+
+	public int criar(Comentario comentario) {
 		return this.daoComentario.create(comentario);
 	}
-	
+
 	public ArrayList<Comentario> list(Integer noticiaId) {
 		return this.daoComentario.list(noticiaId);
 	}
